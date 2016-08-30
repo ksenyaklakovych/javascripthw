e18d2e8ee8bb7dc71c71ad5d28b18de96e733489
Ksenya Klakovych <kklakovych@gmail.com>
2016-08-30 16:00:18 +0000

    added javescript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var a = String(prompt('Enter val a:'));
-var b = String(prompt('Enter val b:'));
-var c = String(prompt('Enter val c:'));
+var a = Number(prompt('Enter val a:'));
+var b = Number(prompt('Enter val b:'));
+var c = Number(prompt('Enter val c:'));
 
 
 var x,x1,x2;
@@ -9,7 +9,7 @@ var d=b*b-4*a*c;
 if ( d < 0 ) {
     document.write('No real roots.');
    } else {
-      if ( d == 0 ) {
+      if ( d === 0 ) {
         var x=-b/2*a;
         document.write('x=',x);
     } else {
